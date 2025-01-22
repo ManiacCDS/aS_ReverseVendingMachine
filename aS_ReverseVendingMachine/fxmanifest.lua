cd840964412c66074cd2f9b95e618207ebae1f3f
aS <98607261+ManiacCDS@users.noreply.github.com>
2025-01-22 16:00:53 +0200

    Add files via upload

---
+++ aS_ReverseVendingMachine/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'aS'
+description 'Reverse Vending Machine Prop for FiveM'
+version '1.0.0'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/as_rv_machine_prop.ydr.ytyp'
